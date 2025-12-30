64d08b4f5c632c2c1ee793c1d5224799b032ff2d
Adrian <loickmarks@gmail.com>
2025-12-30 15:45:05 -0600

    fixing type issue

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
   return <div>
     <h1> Products </h1>
     <div className="grid grid-cols-3 gap-4 p-20 container mx-auto">
-      {products.map(product => (
+      {products.map((product:any) => (
         <div className="bg-white p-5 text-black">
           <h1>{product.name}</h1>
           <img src={product.imageUrl} />
